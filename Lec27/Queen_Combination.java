b08b9d80028ed9453dd773bbb025bbcc7658ae40
monukumar98 <monu012as@gmail.com>
2023-04-25 12:16:11 +0530

    BackTracking-1

--- Lec27/Queen_Combination.java
+++ Lec27/Queen_Combination.java
@@ -15,7 +15,7 @@ public class Queen_Combination {
 			System.out.println(ans);
 			return;
 		}
-		for (int i = 0; i < board.length; i++) {
+		for (int i = idx; i < board.length; i++) {
 			if (board[i] == false) {
 				board[i] = true;
 				Combination(board, tq, qpsf + 1, ans + "b" + i + "q" + qpsf, i + 1);
