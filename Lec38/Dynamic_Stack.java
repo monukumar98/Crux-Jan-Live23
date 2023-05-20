d42f8a2b3ca5098efa74854e35cc55e1bac802fe
monukumar98 <monu012as@gmail.com>
2023-05-20 13:04:18 +0530

    OOps-3

--- Lec38/Dynamic_Stack.java
+++ Lec38/Dynamic_Stack.java
@@ -5,7 +5,7 @@ import Lec37.Stack;
 public class Dynamic_Stack extends Stack {
 	@Override
 	public void push(int item) throws Exception {
-		if (this.isfull()) {
+		if (isfull()) {
 			int[] newarr = new int[2 * arr.length];
 			for (int i = 0; i < arr.length; i++) {
 				newarr[i] = arr[i];
